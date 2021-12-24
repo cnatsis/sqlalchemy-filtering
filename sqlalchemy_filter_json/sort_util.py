96cbb64d0ea4ac272abaf3d32e39d6373bb262f4
Christos Natsis <christos.natsis@incelligent.net>
2021-12-24 19:13:12 +0200

    sorting support on simple fields

--- sqlalchemy_filter_json/sort_util.py
+++ sqlalchemy_filter_json/sort_util.py
@@ -41,25 +41,27 @@ def sort_apply(query, entity, obj: SortRequest = None):
     sort_obj = obj.sort
     if len(sort_obj) != 0:
         for sort_request in sort_obj:
-            node_sort = sort_request.node
+            if sort_request.node is None:
+                node_sort = sort_request.json_field
+            else:
+                node_sort = sort_request.node
+
             sort_split = node_sort.split('.')
             if len(sort_split) == 0:
                 sort_stmt = getattr(entity, sort_request.json_field)[sort_request.node]
             else:
                 sort_stmt = getattr(entity, sort_request.json_field)
                 for s in sort_split:
-                    sort_stmt = sort_stmt[s]
+                    if s is not node_sort:
+                        sort_stmt = sort_stmt[s]
 
             direction = sort_request.direction
             nulls_last = sort_request.nullsLast
 
             if direction is None or direction == SortDirection.ASC:
                 sort_stmt = sort_stmt.asc()
-            elif direction == SortDirection.DESC:
-                sort_stmt = sort_stmt.desc()
             else:
-                # TODO redundant - delete it?
-                raise Exception('Invalid `{}` order by direction'.format(direction))
+                sort_stmt = sort_stmt.desc()
 
             if nulls_last is not None and nulls_last:
                 sort_stmt = nullslast(sort_stmt)
